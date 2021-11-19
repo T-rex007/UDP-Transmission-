d25afaa1c8550ab6a1f0684d464203a4bf8fa8d5
Tyrel Cadogan <shaqc777@yahoo.com>
2021-11-19 06:27:32 -0400

    ----

--- live_video_client.py
+++ live_video_client.py
@@ -28,24 +28,23 @@ message = b'Whyrel'
 client_socket.sendto(message, (host_ip, port))
 fps, st, frames_to_count, cnt = (0, 0, 20, 0)
 while True:
-	packet, _ = client_socket.recvfrom(BUFF_SIZE)
-	data = base64.b64decode(packet, ' /')
-	npdata = np.fromstring(data, dtype=np.uint8)
-	frame = cv2.imdecode(npdata, 1)
+    packet, _ = client_socket.recvfrom(BUFF_SIZE)
+    data = base64.b64decode(packet, ' /')
+    npdata = np.fromstring(data, dtype=np.uint8)
+    frame = cv2.imdecode(npdata, 1)
+    frame = cv2.putText(frame, 'FPS: '+str(fps), (10, 40),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
     frame = cv2.putText(frame, 'FPS: '+str(fps), (10, 40),
-                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-	frame = cv2.putText(frame, 'FPS: '+str(fps), (10, 40),
 	                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
-	cv2.imshow("RECEIVING VIDEO", frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord('q'):
-		client_socket.close()
+    cv2.imshow("RECEIVING VIDEO", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        client_socket.close()
         break
-	if cnt == frames_to_count:
-		try:
-			fps = round(frames_to_count/(time.time()-st))
-			st = time.time()
-			cnt = 0
-		except:
-			pass
-	cnt += 1
+    if cnt == frames_to_count:
+        try:
+            fps = round(frames_to_count/(time.time()-st))
+            st = time.time()
+            cnt = 0
+        except:
+            pass
+    cnt += 1
